f42db6cecb702b912af1b6cba9b8e56b0994c2ea
Lam Nhan <lamnhan92@gmail.com>
2018-05-02 03:01:21 +0700

    update

--- src/configs/sheetbase.config.ts
+++ src/configs/sheetbase.config.ts
@@ -1,38 +1,7 @@
 export const SHEETBASE_CONFIG = {
-
-    /*
-    *   Authentication, using Google API key for public spreadsheet
-    * 
-    * */
-    apiKey: 'AIzaSyC6Rpk_JAZLNc9cp5GxkixfazHdMoYZ8VU', // replace this with your API Key
-
-    /*
-    *   Database address, a spreadsheet id
-    *
-    * */
-    database: '1BkCUWPCVWr90xfOTy5bDCDDbw3fD83XNBWCna3J98JY', // replace this with your database ID
-    
-    /*
-    *   Google script web app address, using as backend
-    *
-    * */
-    backend: '', // replace this with your backend ID
-
-    /*
-    *   (optional) Custom modifiers, modify the data returned from spreadsheet
-    * 
-    * */
-    modifiers: {
-        'posts': posts
-    }
-
-}
-
-
-export function posts(item: any): any {
-    if(!item.categories) item.categories = {
-        uncategorized: 'Uncategorized'
-    };
-
-    return item;
+    // Must be JSON-liked format
+    apiKey: 'SUkepreTR52rAhustEju7haYak4ZUhax',
+    database: '1BkCUWPCVWr90xfOTy5bDCDDbw3fD83XNBWCna3J98JY',
+    backend: 'AKfycbytN8fouXLx-0Yhwwpu3-uXb7fW06ec6mBQFfuslyAZZ-tQvExQ',
+    /*(optional)*/ googleApiKey: 'AIzaSyC6Rpk_JAZLNc9cp5GxkixfazHdMoYZ8VU'
 }
